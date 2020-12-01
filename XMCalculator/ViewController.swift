83fc7fd0e9a10b5dd9589c89a8c3f65992fac9ca
zhuxiaomei <zhuxiaomei.meleny@bytedance.com>
2020-12-01 16:09:00 +0800

    Initial Commit

---
+++ XMCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XMCalculator
+//
+//  Created by bytedance on 2020/12/1.
+//  Copyright © 2020 zhuxiaomei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
